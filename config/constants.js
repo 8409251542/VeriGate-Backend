f7c8b96a0d079b5b49b41f480a1a5ca116c4d5e2
8409251542 <ravi.yoof@gmail.com>
2026-03-27 22:06:35 +0530

    debug

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,8 @@
 const path = require("path");
+const os = require("os");
 
 const COST_PER_VERIFICATION = 0.0011;
-const DEBUG_DIR = path.join(__dirname, "..", "uploads", "verified-data", "debug");
+const DEBUG_DIR = path.join(os.tmpdir(), "nexauth-debug");
 
 module.exports = {
   COST_PER_VERIFICATION,
